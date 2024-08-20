808d639f28933d3e3cae0fe27db6eabc1b8e72b4
Ivan Sapozhnikov <ivan101454@gmail.com>
2024-08-20 06:57:15 +0300

    create gradle project

---
+++ src/main/java/by/ivan101454/Main.java
@@ -0,0 +1,9 @@
+package by.ivan101454;
+
+import org.springframework.util.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("hello word!"));
+    }
+}
